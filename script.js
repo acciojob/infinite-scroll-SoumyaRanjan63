de1c5a622532a3017697fc34bbfb0bb0e5e971ab
SoumyaRanjan63 <130931666+SoumyaRanjan63@users.noreply.github.com>
2023-08-24 10:31:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here!
+const items = document.getElementById("infi-list");
 
+for (let i = 1; i <= 10; i++) {
+    let li = document.createElement('li');
+    li.innerHTML = `item:${i}`;
+    items.appendChild(li);
+}
+
+items.addEventListener('scroll', () => {
+    if (items.scrollTop + items.clientHeight >= items.scrollHeight) {
+        for (let i = 0; i < 2; i++) {
+            let li = document.createElement('li');
+            li.innerHTML = `item:${items.children.length + 1}`;
+            items.appendChild(li);
+        }
+    }
+});
